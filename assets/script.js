82bf7577ec0d88dbc8ce84fce2a5f13437aef623
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-01-23 22:24:47 -0500

    stylesheet css mockup, notes placed with script.js

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// Create var for score, timer (how much time per question), questions
+//WHen a question is answered correctly, move to next question - Answered incorrectly, time is subtracted from clock (timer shouldn't be too short - how much time subtrated?)
+//When all questions answered, the Game is Over - When the timer reaches Zero, the Game is Also Over
+//Initials Saved and Score
+
+//What questions and How many quetions?  Questions are in mutiple choice format
+
+//Start Button to Begin Timer and Game Essentially
