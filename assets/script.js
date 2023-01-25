60ae40402afb1e8634f3eb998b2134a9990306a3
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-01-24 19:51:39 -0500

    commented 5 questions on script.js

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,11 @@
 //What questions and How many quetions?  Questions are in mutiple choice format
 
 //Start Button to Begin Timer and Game Essentially
+
+
+// Questions - Placeholders - Will most likely change
+// Question 1 - What does CSS Stand For?
+// Question 2 - How do you declare a variable in Javascript?
+// Question 3 - What does Math.floor() in Javascript do?
+// Question 4 - All but one are conditional statements within Javascript
+// Question 5 - WHat does HTML Stand For?
