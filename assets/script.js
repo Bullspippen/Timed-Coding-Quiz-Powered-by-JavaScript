8e5b58371c78af19e6cb9942e6d0d9ecba541b34
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-01-24 21:51:52 -0500

    added commas to lines 16 and 21 on script.js

--- assets/script.js
+++ assets/script.js
@@ -13,12 +13,12 @@ let questionsArray = [
         question:'How can a datatype be declared to be a constant type?',
         answersArray:['var', 'constant', 'let','const'],
         correctAnswer:'const'
-    }
+    },
     {
         question:'Which of the following methods is used to access HTML elements using Javascript?',
         answersArray:['getElementById()', 'getElementsByClassName()', 'Both A and B','None of the above'],
         correctAnswer:'Both A and B'
-    }
+    },
     {
         question:'What keyword is used to check whether a given property is valid or not?',
         answersArray:['is in', 'in', 'lies','exists'],
