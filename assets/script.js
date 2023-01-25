c140380c698194cc820b08962a5b33eb38132412
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-01-24 21:40:51 -0500

    HTML, script.js re-structured and configured to allow for game to fucntion as designed however, start button is an issue

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,148 @@
-// Create var for score, timer (how much time per question), questions
-//WHen a question is answered correctly, move to next question - Answered incorrectly, time is subtracted from clock (timer shouldn't be too short - how much time subtrated?)
-//When all questions answered, the Game is Over - When the timer reaches Zero, the Game is Also Over
-//Initials Saved and Score
+let questionsArray = [
+    {
+        question:'Which of the following is not javascript data types?',
+        answersArray:['Null type', 'Undefined type', 'Number type','All of the above'],
+        correctAnswer:'All of the above'
+    },
+    {
+        question:'Which of the following can be used to call a JavaScript Code Snippet?',
+        answersArray:['Function/Method', 'Preprocessor', 'Triggering Event','RMI'],
+        correctAnswer:'Function/Method'
+    },
+    {
+        question:'How can a datatype be declared to be a constant type?',
+        answersArray:['var', 'constant', 'let','const'],
+        correctAnswer:'const'
+    }
+    {
+        question:'Which of the following methods is used to access HTML elements using Javascript?',
+        answersArray:['getElementById()', 'getElementsByClassName()', 'Both A and B','None of the above'],
+        correctAnswer:'Both A and B'
+    }
+    {
+        question:'What keyword is used to check whether a given property is valid or not?',
+        answersArray:['is in', 'in', 'lies','exists'],
+        correctAnswer:'in'
+    }
+]
 
-//What questions and How many quetions?  Questions are in mutiple choice format
+let welcomePage = document.getElementById('welcomePage')
+let questionPage = document.getElementById('questionsPage')
+let scorePage = document.getElementById('scorePage')
 
-//Start Button to Begin Timer and Game Essentially
+let questions = document.getElementById('questions')
+let answers = document.getElementById('answers')
 
+let countdown;
+let timerEl = document.getElementById('timer')
+let time = 60;
+timerEl.textContent = 'Time Left: '+time;
+let secondsLeft;
+let questionIndex = 0;
+let numberCorrect;
+let numberIncorrect;
+let questionArrayOrder;
 
-// Questions - Placeholders - Will most likely change
-// Question 1 - What does CSS Stand For?
-// Question 2 - How do you declare a variable in Javascript?
-// Question 3 - What does Math.floor() in Javascript do?
-// Question 4 - All but one are conditional statements within Javascript
-// Question 5 - WHat does HTML Stand For?
+let scoreEl = document.getElementById('score')
+let finalScore =0
+
+let inputEl = document.getElementById('inputInitials')
+
+let sectionsArray = [welcomePage, questionPage, scorePage]
+
+function hidePages(){
+    for (let i = 0; i < sectionsArray.length; i++) {
+        if(!sectionsArray[i].classList.contains('hide')){
+            sectionsArray[i].classList.add('hide')
+        }
+        
+    }
+}
+
+function questionOrder(){
+    let arrayIndex = [];
+    for (let i = 0; i < questionsArray.length; i++) {
+        arrayIndex.push(i)
+    }
+
+    return arrayIndex;
+}
+
+function startQuiz(){
+    hidePages()
+    questionPage.classList.remove('hide');
+
+    secondsLeft = time;
+
+    countdown = setInterval(() => {
+        timerEl.textContent = 'Time Left: '+ secondsLeft;
+        secondsLeft--;
+
+        if(secondsLeft < 0){
+            clearInterval(countdown)
+            secondsLeft = time;
+        }
+    }, 1000)
+    
+    questionArrayOrder = questionOrder(questionsArray)
+
+    questionDisplay(questionsArray, questionArrayOrder[questionIndex])
+}
+
+function questionDisplay(arr, index){
+    questions.textContent = arr[index].question
+
+    let ans;
+    let but;
+    let relativeAnswers = arr[index].answersArray;
+
+    for (let i = 0; i < relativeAnswers.length; i++) {
+        ans = document.createElement('LI')
+        but = document.createElement('button')
+        
+        but.textContent = relativeAnswers[i]
+        ans.addEventListener('click', nextQuestion)
+        ans.appendChild(but)
+        answers.appendChild(ans)
+    }
+
+}
+
+function nextQuestion(event){
+    let answerChosen = event.target.textContent
+    let rightAnswer = questionsArray[questionArrayOrder[questionIndex]].correctAnswer
+
+    if( answerChosen === rightAnswer){
+        numberCorrect++
+    }else{
+        secondsLeft -= 10;
+        numberIncorrect++
+    }
+    timerEl.textContent = 'Time Left: '+ secondsLeft;
+
+    questionIndex++;
+
+    clearPreviousQuestion()
+
+    if(questionIndex < questionsArray.length){
+        questionDisplay(questionsArray, questionArrayOrder[questionIndex])
+    }
+    else{
+        finalScore = secondsLeft;
+        scoreEl.textContent = finalScore
+        scorePage.classList.remove('hide')
+        clearInterval(countdown)
+    }
+    
+}
+
+function clearPreviousQuestion(){
+    questions.textContent = '';
+    while(answers.hasChildNodes()){
+        answers.removeChild(answers.childNodes[0])
+    }
+}
+
+function submitInitials(){
+    console.log(inputEl.value)
+}
