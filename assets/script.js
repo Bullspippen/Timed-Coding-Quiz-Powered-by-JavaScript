2f6be6cd09efe02046fd1dc115fe08ee7460ae46
Mohamed Osman <mohamed.osman111681@gmail.com>
2023-03-12 23:03:41 -0400

    created questions.js file and included import function within script.js file

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,4 @@
-let questionsArray = [
-    {
-        question:'Which of the following is not javascript data types?',
-        answersArray:['Null type', 'Undefined type', 'Number type','All of the above'],
-        correctAnswer:'All of the above'
-    },
-    {
-        question:'Which of the following can be used to call a JavaScript Code Snippet?',
-        answersArray:['Function/Method', 'Preprocessor', 'Triggering Event','RMI'],
-        correctAnswer:'Function/Method'
-    },
-    {
-        question:'How can a datatype be declared to be a constant type?',
-        answersArray:['var', 'constant', 'let','const'],
-        correctAnswer:'const'
-    },
-    {
-        question:'Which of the following methods is used to access HTML elements using Javascript?',
-        answersArray:['getElementById()', 'getElementsByClassName()', 'Both A and B','None of the above'],
-        correctAnswer:'Both A and B'
-    },
-    {
-        question:'What keyword is used to check whether a given property is valid or not?',
-        answersArray:['is in', 'in', 'lies','exists'],
-        correctAnswer:'in'
-    }
-]
+import { questionsArray } from './questions.js';
 
 let welcomePage = document.getElementById('welcomePage')
 let questionPage = document.getElementById('questionsPage')
